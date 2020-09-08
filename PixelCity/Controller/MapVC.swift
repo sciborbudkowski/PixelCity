1849eb62e83de436cb113bb2bb5ce43877f1010f
Ścibor Budkowski <scibor.budkowski@gmail.com>
2020-09-08 14:38:43 +0200

    localization services set

--- PixelCity/Controller/MapVC.swift
+++ PixelCity/Controller/MapVC.swift
@@ -8,22 +8,52 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class MapVC: UIViewController {
 
     @IBOutlet weak var mapView: MKMapView!
     
+    var locationManager = CLLocationManager()
+    let authorizationStatus = CLLocationManager.authorizationStatus()
+    let regionRadius: Double = 1000
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         mapView.delegate = self
+        locationManager.delegate = self
+        
+        configureLocationServices()
     }
 
     @IBAction func centerMapPressed(_ sender: Any) {
+        if authorizationStatus == .authorizedAlways || authorizationStatus == .authorizedWhenInUse {
+            centerMapOnUserLocation()
+        }
     }
     
 }
 
 extension MapVC: MKMapViewDelegate {
+    func centerMapOnUserLocation() {
+        guard let coordinate = locationManager.location?.coordinate else { return }
+        let coordinateRegion = MKCoordinateRegion(center: coordinate, latitudinalMeters: regionRadius * 2.0, longitudinalMeters: regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+}
+
+extension MapVC: CLLocationManagerDelegate {
     
+    func configureLocationServices() {
+        if authorizationStatus == .notDetermined {
+            locationManager.requestAlwaysAuthorization()
+        } else {
+            return
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        centerMapOnUserLocation()
+    }
 }
